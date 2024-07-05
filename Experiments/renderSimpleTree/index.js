26dd3ddf0394229b7ec3882f4e80bc58610c173c
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-07-04 19:46:41 -0600

    importing obj load from script tag

--- Experiments/renderSimpleTree/index.js
+++ Experiments/renderSimpleTree/index.js
@@ -1,5 +1,3 @@
-import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/jsm/loaders/OBJLoader.js';
-
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -14,15 +12,15 @@ light.position.set(10, 20, 10);
 scene.add(light);
 
 // Load the house model
-const objLoader = new OBJLoader();
-objLoader.load('./House.obj', function (object) {
+const objLoader = new THREE.OBJLoader();
+objLoader.load('./models/House.obj', function (object) {
     object.scale.set(0.5, 0.5, 0.5);
     object.position.set(0, 0, -5);
     scene.add(object);
 });
 
 // Load the tree model
-objLoader.load('./Tree.obj', function (object) {
+objLoader.load('./models/Tree.obj', function (object) {
     object.scale.set(0.5, 0.5, 0.5);
     object.position.set(-2, 0, -5);
     scene.add(object);
