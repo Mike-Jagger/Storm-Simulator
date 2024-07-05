af7ee032dedf343269dcfd8bd49be8260d7fe70c
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-07-05 15:51:59 -0600

    removing wrongly copied file name

--- Experiments/precipitation/index.js
+++ Experiments/precipitation/index.js
@@ -1,4 +1,4 @@
-Experiments/RotationAnimation/index.html// Set up the scene, camera, renderer, and controls
+// Set up the scene, camera, renderer, and controls
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
